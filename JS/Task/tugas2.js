4a2c4ef6005e64abb2d610136614f8e7054fbe80
devfm <devfm.go@gmail.com>
2020-12-09 20:22:45 +0700

    update task tugas2.js

---
+++ JS/Task/tugas2.js
@@ -0,0 +1,16 @@
+let age = 26;
+let userName = "Muhamad Fikri";
+let data = 6;
+if (data < 10) {
+    result = false;
+} else {
+    result = true;
+}
+let nullData = null;
+let alfabet = ["A","B","C","D"];
+
+console.log(age);
+console.log(userName);
+console.log(result);
+console.log(nullData);
+console.log(alfabet);
