6e429342e1d18bfa394318b862f196f68c38fddd
devfm <devfm.go@gmail.com>
2020-12-12 20:53:47 +0700

    update tugas4

--- JS/Task/tugas4.js
+++ JS/Task/tugas4.js
@@ -1,22 +1,35 @@
-var x = 160;
-var y = 165;
+var x = 180;
+var y = 185;
 var z = 170;
-if (x > y && x > z) {
-  if (y > z) {
-    console.log(x + ", " + y + ", " + z);
-  } else {
-    console.log(x + ", " + z + ", " + y);
-  }
-} else if (y > x && y > z) {
-  if (x > z) {
-    console.log(y + ", " + x + ", " + z);
-  } else {
-    console.log(y + ", " + z + ", " + x);
-  }
-} else if (z > x && z > y) {
-  if (x > y) {
-    console.log(z + ", " + x + ", " + y);
-  } else {
-    console.log(z + ", " + y + ", " + x);
-  }
+ function maxTinggiSiswa(){
+     if (x > y && x > z) {
+        console.log("siswa x lebih tinggi");
+     }else if(y > x && y > z){
+        console.log("siswa y lebih tinggi");
+     }else{
+        console.log("siswa z lebih tinggi");
+     }
+ }
+function sortUrutanTinggi() {
+    if (x > y && x > z) {
+      if (y > z) {
+        console.log(x + ", " + y + ", " + z);
+      } else {
+        console.log(x + ", " + z + ", " + y);
+      }
+    } else if (y > x && y > z) {
+      if (x > z) {
+        console.log(y + ", " + x + ", " + z);
+      } else {
+        console.log(y + ", " + z + ", " + x);
+      }
+    } else if (z > x && z > y) {
+      if (x > y) {
+        console.log(z + ", " + x + ", " + y);
+      } else {
+        console.log(z + ", " + y + ", " + x);
+      }
+    }
 }
+ maxTinggiSiswa();
+ sortUrutanTinggi();
