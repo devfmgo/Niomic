8fa8f100cd346f0acf7bcd9f3ea7cfbdc3668cb7
devfm <devfm.go@gmail.com>
2020-12-12 19:39:07 +0700

    update learning javascript

---
+++ JS/Excercise/forJavascript.js
@@ -0,0 +1,6 @@
+function forJavascript() {
+  for (let i = 0; i < 10; i++) {
+    console.log("Jalan :", i);
+  }
+}
+forJavascript();
---
+++ JS/Excercise/infinityLoopJavascript.js
@@ -0,0 +1,15 @@
+/**
+ * infinity loop
+ * 1. gunakan perulangan for
+ * 2. menggunakan while
+ */
+
+// for (var i = 0; i >= 0; i++) {
+//   console.log(i);
+// }
+
+var i = 1;
+while (true) {
+  console.log(i);
+  i++;
+}
